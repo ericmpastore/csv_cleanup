035e2ac289c2a122ad808a8d4865669ddc9489dd
Eric P <epas001@hotmail.com>
2026-02-17 07:21:45 -0500

    Finished adding null handling methods to clean_nulls function

--- cloud_cleaning.py
+++ cloud_cleaning.py
@@ -31,16 +31,22 @@ def clean_nulls(input_frame):
     null_count = mask.sum()
     output_frame.loc[mask,'vm_id'] = [f'zzzz-zzzz-{i:04d}' for i in range(1,null_count+1)]
 
-    # Handle numerical nulls using fillna(mean), EPastore 02162026
+    # Handle numerical nulls using fillna(mean), EPastore 02172026
     output_frame['cpu_usage'] = output_frame['cpu_usage'].fillna(output_frame['cpu_usage'].mean())
     output_frame['memory_usage'] = output_frame['memory_usage'].fillna(output_frame['memory_usage'].mean())
+    output_frame['network_traffic'] = output_frame['network_traffic'].fillna(output_frame['network_traffic'].mean())
+    output_frame['power_consumption'] = output_frame['power_consumption'].fillna(output_frame['power_consumption'].mean())
+    output_frame['num_executed_instructions'] = output_frame['num_executed_instructions'].fillna(output_frame['num_executed_instructions'].mean())
+    output_frame['execution_time'] = output_frame['execution_time'].fillna(output_frame['execution_time'].mean())
+    output_frame['energy_efficiency'] = output_frame['energy_efficiency'].fillna(output_frame['energy_efficiency'].mean())
 
     # Handle timestamp nulls using the backwards fill method, EPastore 02162026
     output_frame['timestamp'] = output_frame['timestamp'].bfill()
 
-    # Handle task field nulls
-
-    # Handle remaining nulls
+    # Handle task field nulls with 'Unknown', EPastore 02172026
+    output_frame['task_type'] = output_frame['task_type'].fillna('unknown')
+    output_frame['task_priority'] = output_frame['task_priority'].fillna('unknown')
+    output_frame['task_status'] = output_frame['task_status'].fillna('unknown')
 
     return output_frame
 
@@ -53,7 +59,7 @@ def main():
     EPastore, 02/07/2026
     """
     clean_frame = clean_nulls(get_frame('cloud_data.csv'))
-    # print(clean_nulls(get_frame('cloud_data.csv').head(100)))
+    print(clean_nulls(get_frame('cloud_data.csv').head(100)))
     # print(get_frame('cloud_data.csv').describe())
     # print(clean_frame.describe())
     clean_frame.to_csv('cleaned_data.csv')
